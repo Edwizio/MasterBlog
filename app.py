0a5f8cf303848d826236545b25917e97eb784d95
Zahra Rauf <zahra.aleem@gmail.com>
2025-09-19 09:56:21 +0200

    templates and style sheets created and linked with app.py to display the json file

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
+import json
+from flask import Flask, render_template, request
 
-from flask import Flask
+def load_data():
+    """
+    This functions takes a json file as parameter and returns its data as a string
+    :return data:
+    """
+    with open("data.json", "r") as reader:
+        data = json.loads(reader.read())
+
+    return data
+
+blog_posts = load_data()
 
 app = Flask(__name__)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        pass
+    return render_template('add.html')
+
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+
+    blog_posts = load_data()
+
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
